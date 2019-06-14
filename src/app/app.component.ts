64975582c72ba9793d9710adb2a9a041398bc714
samuel <samuel.silv.carvalho@gmail.com>
2019-06-13 22:42:59 -0300

    configuraao do carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import 'hammerjs';
+import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'card-amanda';
+
+  @ViewChild("carousel") carousel: NgbCarousel;
+
+  nextCard() {
+    this.carousel.next();
+  }
+
+  previousCard() {
+    this.carousel.prev();
+  }
+
+  images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
 }
